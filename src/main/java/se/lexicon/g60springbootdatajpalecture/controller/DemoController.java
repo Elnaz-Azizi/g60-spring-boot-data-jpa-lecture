a33a06a8c17263fd3df7900782f1ec1092448939
Elnaz Azizi <azizi.elnaz66@gmail.com>
2026-06-09 11:07:38 +0200

    Add POST endpoint to DemoController for handling dynamic request body data
    - Introduced a new POST endpoint in `DemoController` to handle dynamic request body inputs and return a structured response with additional metadata such as `id`, `name`, `age`, and `createDate`.

--- src/main/java/se/lexicon/g60springbootdatajpalecture/controller/DemoController.java
+++ src/main/java/se/lexicon/g60springbootdatajpalecture/controller/DemoController.java
@@ -1,21 +1,48 @@
 package se.lexicon.g60springbootdatajpalecture.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 
 @RestController
+@RequestMapping("/api/v1/demo")
 public class DemoController {
 
-    @GetMapping("/api/v1/demo")
+    @GetMapping
     public ResponseEntity<String> index() {
         System.out.println("### index method called ###");
         String responseBody = "Hello World!";
         return ResponseEntity.ok(responseBody); //200
     }
 
+    @PostMapping
+    /*public ResponseEntity <Map<String, Object>> create() {
+        System.out.println("### create method called ###");
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("id", 1);
+        responseBody.put("name","Student Name");
+        responseBody.put("createDate", LocalDate.now());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);}*/
 
+        /*
+        id: 1
+        name: "Student Name"
+        createDate: 2026-06-09
+        */
+
+    public ResponseEntity <Map<String, Object>> create(@RequestBody Map<String, Object> requestBody) {
+        System.out.println("### create method called ###");
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("id", 1);
+        responseBody.put("name", requestBody.get("name"));
+        responseBody.put("age", requestBody.get("age"));
+        responseBody.put("createDate", LocalDate.now());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+
+    }
 }
